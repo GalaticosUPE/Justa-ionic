b7a54d0fa8e8625b4a3ae7cf366a7076638cdd0a
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2023-04-22 18:32:49 -0300

    All quiet firebase e angular
    até agora, tudo certo

--- Project-00/TryApp-0/src/environments/environment.ts
+++ Project-00/TryApp-0/src/environments/environment.ts
@@ -3,7 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  //firebase web
+
+  firebase: {
+    apiKey: "AIzaSyAlIWMVhDOd5_ZqqsrkAAkNf1g2RIa6qL4",
+    authDomain: "tentativa-1-lpi.firebaseapp.com",
+    projectId: "tentativa-1-lpi",
+    storageBucket: "tentativa-1-lpi.appspot.com",
+    messagingSenderId: "145594842029",
+    appId: "1:145594842029:web:1c10144c0c0599e077bf24",
+    measurementId: "G-WSDXRT1QHS"
+  }
+  
 };
 
 /*
