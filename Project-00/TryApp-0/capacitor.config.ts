c78a44016b2820c64bb57401e68073f8e08e3268
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2023-04-27 00:09:14 -0300

    Mobile permission
    enviando permissões para o mobile

--- Project-00/TryApp-0/capacitor.config.ts
+++ Project-00/TryApp-0/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'TryApp-0',
+  appName: 'Justa Galactus',
   webDir: 'www',
   bundledWebRuntime: false
 };
